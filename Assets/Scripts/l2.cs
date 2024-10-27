a177d319d0adcc39df958a679ae94ff0314c08b3
Ayah-Ayyash <ayahayyash710@gmail.com>
2024-10-27 11:19:45 +0200

    first commit

---
+++ Assets/Scripts/l2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class l2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     int playerLives = 3;
+
+      while (playerLives > 0)
+        {
+            // Check the number of lives and display messages accordingly
+            if (playerLives == 3)
+            {
+                Debug.Log("Many lives");
+            }
+            else if (playerLives == 1)
+            {
+                Debug.Log("The last life!");
+            }
+
+            // Reduce the player's lives by 1
+            playerLives--;
+
+            // If playerLives reaches 0, display the game over message
+            if (playerLives == 0)
+            {
+                Debug.Log("Game Over!");
+            }
+        }
+    }
+}
+
+
+    
+
+
+
